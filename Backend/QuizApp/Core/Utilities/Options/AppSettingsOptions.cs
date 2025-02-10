ce61b6da8318902398cce4f4bdc66fd8b2df2e8c
mberkayakardev <mberkayakar@outlook.com>
2025-02-10 22:48:23 +0300

    jwt operasyonları eklendi

--- Backend/QuizApp/Core/Utilities/Options/AppSettingsOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace TrendMusic.ECommerce.Core.Utilities.Options
-//{
-//    public class AppSettingsOptions
-//    {
-//        public string ConnectionString { get; set; }
-//        public bool MainterenceMode { get; set; }
-//        public string DefaultProjectTheme { get; set; } // Varsayılan proje teması eklendi. 
-//    }
-//}
--- Backend/QuizApp/Core/Utilities/Results/Api/BaseResult/IDataResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core.Utilities.Results.MVC.BaseResult
-{
-    public interface IDataResult<T> : IResult
-    {
-        T Data { get; }
-
-
-    }
-}
